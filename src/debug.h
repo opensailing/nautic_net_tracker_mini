9f01212bdc1a387793e44b40eeff33dd86bcb00d
Rockwell Schrock <rockwell@schrock.me>
2023-04-28 14:55:53 -0400

    Add debugln2() macro

--- src/debug.h
+++ src/debug.h
@@ -6,13 +6,15 @@
 #ifdef SERIAL_DEBUG
 #define debugWait() \
     while (!Serial) \
-    delay(1)
+    delay(10)
 #define debug(x) Serial.print(x)
 #define debugln(x) Serial.println(x)
+#define debugln2(x, y) Serial.println(x, y)
 #else
 #define debugWait()
 #define debug(x)
 #define debugln(x)
+#define debugln2(x, y)
 #endif
 
 #endif
